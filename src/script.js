4953ef64174162cc55b11df0311f1069522a6c43
Abderrahmanlahmidi <Abderrahmanlahmidi@github.com>
2024-11-18 09:26:25 +0100

    feat:fetch data

--- src/script.js
+++ src/script.js
@@ -1,25 +1,48 @@
-const lists = document.querySelectorAll(".list");
-const boxes = document.querySelectorAll(".box");
-const data_box = document.querySelectorAll(".data_box");
+let lists = document.querySelectorAll(".list");
+let boxes = document.querySelectorAll(".box");
+let full_name = document.querySelectorAll(".full_name");
+let post_title = document.querySelectorAll(".post_title");
+let birthday = document.querySelectorAll(".birthday");
+
+const fetchData = async () => {
+    const jsonFileUrl = "./data.json";
+
+    try {
+        const response = await fetch(jsonFileUrl);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
 
 
-for (let i = 0; i < lists.length; i++) {
-
-    lists[i].addEventListener("dragstart", function(e){
-        let selected = e.target;
-
-        boxes.forEach(element => {
-            element.addEventListener("dragover", function(e){
-                e.preventDefault();
-
-            });
-            element.addEventListener("drop", function(){
-                element.appendChild(selected);
-                selected = null;
-            })
-
+        data.team.forEach((member, index) => {
+            if (full_name[index]) full_name[index].textContent = member.name;
+            if (post_title[index]) post_title[index].textContent = member.post;
+            if (birthday[index]) birthday[index].textContent = member.birthday;
         });
 
-    })
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+    }
+};
+
+fetchData();
+
+lists.forEach(list => {
     
-}
+    list.addEventListener("dragstart", function (e) {
+        const selected = e.target;
+
+        boxes.forEach(box => {
+            box.addEventListener("dragover", function (e) {
+                e.preventDefault();
+            });
+
+            box.addEventListener("drop", function () {
+                box.appendChild(selected);
+            });
+        });
+    });
+});
