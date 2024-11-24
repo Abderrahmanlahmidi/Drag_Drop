a337d3ee92e2134e840ee67524780974f0b954c3
Lahmidi <121018955+Abderrahmanlahmidi@users.noreply.github.com>
2024-11-24 09:47:34 +0100

    feat:code

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 let lists = document.querySelector(".list");
 let searchBar = document.getElementById("searchBar");
-let profilePersonData = []; 
+let box_1 = document.querySelector(".box_1");
+let box_2 = document.querySelector(".box_2");
+let box_3 = document.querySelector(".box_3");
+let box_4 = document.querySelector(".box_4");
+let profilePersonData = [];
 
 
 const loadData = async () => {
@@ -8,6 +12,7 @@ const loadData = async () => {
         const res = await fetch("./data.json");
         profilePersonData = await res.json(); 
         addData(profilePersonData.team);
+        
     } catch (err) {
         console.error("Error loading data:", err);
     }
@@ -17,24 +22,30 @@ const loadData = async () => {
 const addData = (teamData) => {
     const htmlString = teamData.map((element) => {
         return `
-        <div draggable="true" class="max-w-xs cursor-pointer rounded-lg border-[1px] border-gray-700 dark:border-gray-600 bg-gray-800 dark:bg-gray-800">
+        <div draggable="true" class="draggable max-w-xs cursor-all-scroll rounded-lg border-[1px] border-gray-700 dark:border-gray-600 bg-gray-800 dark:bg-gray-800" 
+             data-post="${element.post}">
           <div class="px-6 py-4">
              <div class="full_name font-semibold text-2xl mb-2 text-blue-400">${element.name}</div>
              <p class="post_title text-white dark:text-gray-200 text-lg">${element.post}</p>
              <p class="text-gray-400 dark:text-gray-300 text-sm">Birthday: <span class="font-medium birthday">${element.birthday}</span></p>
           </div>
-         </div>
+        </div>
         `;
     }).join(' ');
 
     lists.innerHTML = htmlString;
+    enableDragAndDrop();
 };
+
+
 
 searchBar.addEventListener("keyup", (e) => {
     const searchString = e.target.value.toLowerCase();
     const filteredData = profilePersonData.team.filter((item) => {
         return (
-            item.name.toLowerCase().includes(searchString) 
+            item.name.toLowerCase().includes(searchString) ||
+            item.post.toLowerCase().includes(searchString) ||
+            item.birthday.toLowerCase().includes(searchString)
         );
     });
     addData(filteredData);
@@ -45,10 +56,7 @@ loadData();
 
 
 
-
-
-
-
-
-
+function dragDrop(){
+    
+}
 
